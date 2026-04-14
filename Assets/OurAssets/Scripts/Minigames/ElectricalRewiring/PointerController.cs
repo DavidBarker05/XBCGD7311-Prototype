5de3bf9dbca394fc0b691da625e8f5bfb00c31ac
LazyTallIndian <77723138+Abhijith2111@users.noreply.github.com>
2026-04-14 13:10:19 +0200

    Added in slider QTE

---
+++ Assets/OurAssets/Scripts/Minigames/ElectricalRewiring/PointerController.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class PointerController : MonoBehaviour
+{
+
+    public Transform pointA;
+    public Transform pointB;
+    public RectTransform safeZone;
+    public float moveSpeed = 100f;
+
+    private float direction = 1f;
+    private RectTransform pointerTransform;
+    private Vector3 targetPosition;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        pointerTransform = GetComponent<RectTransform>();
+        targetPosition = pointB.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointerTransform.position = Vector3.MoveTowards(pointerTransform.position, targetPosition, moveSpeed * Time.deltaTime);
+
+        if(Vector3.Distance(pointerTransform.position, pointA.position) < 0.1f)
+        {
+            targetPosition = pointB.position;
+            direction = 1f;
+        }
+
+        else if(Vector3.Distance(pointerTransform.position, pointB.position) < 0.1f)
+        {
+            targetPosition = pointA.position;
+            direction = -1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CheckSuccess();
+        }
+    }
+
+    void CheckSuccess()
+    {
+        if (RectTransformUtility.RectangleContainsScreenPoint(safeZone, pointerTransform.position, null))
+        {
+            Debug.Log("Success!");
+        }
+        else
+        {
+            Debug.Log("Failure!");
+        }
+    }
+}
