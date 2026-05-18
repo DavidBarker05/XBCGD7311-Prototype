ef635c08786963fb02696484f4a33bef218205f2
DavidBarker05 <davidbarker9611@gmail.com>
2026-05-18 14:40:04 +0200

    code to display number of minigames beaten

---
+++ Assets/OurAssets/Scripts/Ui/MinigamesBeatenDisplayer.cs
@@ -0,0 +1,17 @@
+using TMPro;
+using UnityEngine;
+
+[RequireComponent(typeof(TMP_Text))]
+public class MinigamesBeatenDisplayer : MonoBehaviour
+{
+    [SerializeField]
+    Player m_Player;
+    [SerializeField, TextArea]
+    string m_DisplayText = "Minigames Beaten: {0:D}";
+
+    TMP_Text m_Text;
+
+    void Awake() => m_Text = GetComponent<TMP_Text>();
+
+    void Update() => m_Text.text = string.Format(m_DisplayText, m_Player.MinigamesBeaten);
+}
