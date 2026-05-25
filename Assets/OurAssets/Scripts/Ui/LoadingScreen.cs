cde6aeaea5c6616748ea923c827dfd4eaab93c31
DavidBarker05 <davidbarker9611@gmail.com>
2026-05-25 19:15:21 +0200

    start menu

--- Assets/OurAssets/Scripts/Ui/LoadingScreen.cs
+++ Assets/OurAssets/Scripts/Ui/LoadingScreen.cs
@@ -3,14 +3,23 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class LoadingScreen : MonoBehaviour
 {
+    [SerializeField]
+    Sprite[] m_LoadingScreenImages;
     [field: SerializeField, Min(0)]
     public int SceneIndexToLoad { get; set; } = 0;
     [SerializeField]
     Slider m_LoadingBar;
 
-    void OnEnable() => StartCoroutine(LoadAsync());
+    void OnEnable()
+    {
+        Sprite sprite = null;
+        if (m_LoadingScreenImages.Length > 0) sprite = m_LoadingScreenImages[Random.Range(0, m_LoadingScreenImages.Length)];
+        GetComponent<Image>().sprite = sprite;
+        StartCoroutine(LoadAsync());
+    }
 
     IEnumerator LoadAsync()
     {
