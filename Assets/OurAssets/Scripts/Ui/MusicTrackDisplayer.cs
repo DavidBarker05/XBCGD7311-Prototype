a3c582166f5f5560e472874f7b25bc06753cb79c
DavidBarker05 <davidbarker9611@gmail.com>
2026-09-25 17:53:33 +0200

    should hopefully fix music displayer size

--- Assets/OurAssets/Scripts/Ui/MusicTrackDisplayer.cs
+++ Assets/OurAssets/Scripts/Ui/MusicTrackDisplayer.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
+[RequireComponent(typeof(RectTransform))]
 public class MusicTrackDisplayer : MonoBehaviour
 {
     [SerializeField]
@@ -11,11 +13,16 @@ public class MusicTrackDisplayer : MonoBehaviour
     [SerializeField]
     float m_DisplayDuration = 10f;
 
+    RectTransform m_RectTransform;
+
     public MusicTrack CurrentMusicTrack { get; set; }
+
+    void Awake() => m_RectTransform = GetComponent<RectTransform>();
 
     void OnEnable()
     {
         m_Text.text = string.Format(m_DisplayFormat, CurrentMusicTrack.SongName, CurrentMusicTrack.ArtistName);
+        LayoutRebuilder.ForceRebuildLayoutImmediate(m_RectTransform);
         StartCoroutine(Disappear());
     }
 
