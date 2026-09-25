d94997bd8c8f20d3334cd944fcacdc79973d70e0
DavidBarker05 <davidbarker9611@gmail.com>
2026-09-26 00:06:28 +0200

    fix music stuff

--- Assets/OurAssets/Scripts/Ui/MusicTrackDisplayer.cs
+++ Assets/OurAssets/Scripts/Ui/MusicTrackDisplayer.cs
@@ -1,10 +1,11 @@
 using System.Collections;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(RectTransform))]
-public class MusicTrackDisplayer : MonoBehaviour
+[RequireComponent(typeof(RectTransform), typeof(CanvasGroup))]
+public class MusicTrackDisplayer : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField]
     TMP_Text m_Text;
@@ -14,14 +15,21 @@ public class MusicTrackDisplayer : MonoBehaviour
     float m_DisplayDuration = 10f;
 
     RectTransform m_RectTransform;
+    CanvasGroup m_CanvasGroup;
 
     public MusicTrack CurrentMusicTrack { get; set; }
 
-    void Awake() => m_RectTransform = GetComponent<RectTransform>();
+    void Awake()
+    {
+        m_RectTransform = GetComponent<RectTransform>();
+        m_CanvasGroup = GetComponent<CanvasGroup>();
+    }
 
     void OnEnable()
     {
+        m_CanvasGroup.alpha = 1f;
         m_Text.text = string.Format(m_DisplayFormat, CurrentMusicTrack.SongName, CurrentMusicTrack.ArtistName);
+        m_Text.ForceMeshUpdate();
         LayoutRebuilder.ForceRebuildLayoutImmediate(m_RectTransform);
         StartCoroutine(Disappear());
     }
@@ -31,4 +39,8 @@ public class MusicTrackDisplayer : MonoBehaviour
         yield return new WaitForSecondsRealtime(m_DisplayDuration);
         gameObject.SetActive(false);
     }
+
+    public void OnPointerEnter(PointerEventData eventData) => m_CanvasGroup.alpha = 0f;
+
+    public void OnPointerExit(PointerEventData eventData) => m_CanvasGroup.alpha = 1f;
 }
